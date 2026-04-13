45911a8c10772400574b172c75206dcf89422591
9727290-a11y <9727289@stud.op.edu.ua>
2026-04-13 16:23:27 +0300

    fixed mistakes

--- third_homework.py
+++ third_homework.py
@@ -1,46 +1,37 @@
-#tsk1
-user_exit = input(" press q if you wanna exit or any other symbol to start: ")
-calculator_operation_existing = {
-    "plus": "+",
-    "minus": "-",
-    "multiply": "*",
-    "devide": "/",
-}
 
-while user_exit != "q":
-    first_user_num = int(input("Enter your first number: "))
-    second_user_num = int(input("Enter your second number: "))
-    calculator_operation = input("Enter what operation you want to do (+, -, *, /) : ")
-    if calculator_operation == "+":
-        print(f'{first_user_num} + {second_user_num} =  {first_user_num+second_user_num}')
-    elif calculator_operation == "-":
-        print(f'{first_user_num} - {second_user_num} = {first_user_num-second_user_num}')
-    elif calculator_operation == "/":
-        print(f'{first_user_num} / {second_user_num} = {first_user_num/second_user_num}')
-    elif calculator_operation == "*":
-        print(f'{first_user_num} * {second_user_num} = { first_user_num*second_user_num}')
-    elif calculator_operation not in calculator_operation_existing:
-            print("invalid syntax. operation not existing yet. ")
-            continue
+while True:
+    first_num = int(input("Enter your first number: "))
+    second_num = int(input("Enter your second number: "))
+    arithmetic_operation = input("Enter what operation you want to do (+, -, *, /) : ")
+    if arithmetic_operation == "+":
+        print(f'{first_num} + {second_num} =  {first_num+second_num}')
+    elif arithmetic_operation == "-":
+        print(f'{first_num} - {second_num} = {first_num-second_num}')
+    elif arithmetic_operation == "/" and second_num != 0:
+        print(f'{first_num} / {second_num} = {first_num/second_num}')
+    elif arithmetic_operation == "*":
+        print(f'{first_num} * {second_num} = { first_num*second_num}')
     else:
-        print("that was not an integer")
+        print("invalid syntax. the input is incorrect or unsupported. ")
         continue
 
+    exit_choice = input("\nDo you want to continue? (y/n): ").lower()
+    if exit_choice != 'y':
+        print("Have a great day!")
+        break
 #tsk2
 
 user_balance = int(input("Enter your current balance: "))
-user_expanses_list = {
-}
+expenses = {}
 
-while user_exit != "q":
-    expanses_category = input("Choose category you spent money on or press q to exit: ").casefold()
-    amount_of_expanses = int(input(f"Write how much you spent on {expanses_category}: "))
-    if expanses_category != 0 or "" and amount_of_expanses <= user_balance:
-        user_expanses_list.update({expanses_category : amount_of_expanses})
-        if user_balance > amount_of_expanses:
-            user_balance = user_balance - amount_of_expanses
-            print(f"Done! Your current balance: {user_balance}")
-        elif user_balance < amount_of_expanses:
-            print(f"sorry, insufficient funds for {expanses_category}")
+while True:
+    category = input("Choose category you spent money on or press q to exit: ").casefold()
+    amount = int(input(f"Write how much you spent on {category}: "))
+    if category and amount <= user_balance:
+        expenses[category] = expenses.get(category, 0) + amount
+        continue
 
-            continue
+    exit_choice = input("\nDo you want to continue? (y/n): ").lower()
+    if exit_choice != 'y':
+        print("Have a great day!")
+        break
