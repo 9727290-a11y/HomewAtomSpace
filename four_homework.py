ea97291fd7b573577b112346c60da6771df56e50
MariaLysiuk <9727290@stud.op.edu.ua>
2026-05-13 10:53:51 +0300

    Refactor get_user_info function for clarity, fixed bags

--- four_homework.py
+++ four_homework.py
@@ -1,73 +1,71 @@
-def get_user_info() -> tuple[str, str, str]: 
-            global exit_choice 
+def get_user_info() -> tuple[str, str, str]:
+    while True:
+        full_name = input("Please, enter your full name (at least 2 symbols in each word): ")
+        show_only_initials = input("Show only initials? (y/n): ").lower() == 'y'
+        valid_name = validate_name(full_name, show_only_initials)
+        if valid_name:
+            break
 
-            global full_name
-            full_name = input("Please, enter your full name (at least 2 symbols in each word): ")
-            choice_initials = input("Show only initials? (y/n): ").lower()
-            show_only_initials = choice_initials == 'y' 
-            valid_name = validate_name(full_name, show_only_initials)
+    while True:
+        email = input("Enter your email: ")
+        mask_email = input("Would you like to mask email? (y/n)").lower() == 'y'
+        valid_email = validate_email(email, mask_email)
+        if valid_email:
+            break
 
-            global email
-            email = input("Enter your email: ")
-            choice_mask = input("Would you like to mask email? (y/n)").lower()
-            mask_email = choice_mask == 'y'
-            valid_email = validate_email(email, mask_email)
+    while True:
+        password = input("Enter yout password(at least 8 symbols): ")
+        encrypt_paassword = input("Would you like to encrypt password? (y/n)").lower() == 'y'            
+        valid_password = validate_password(password, encrypt_paassword)
+        if valid_password:
+            break
 
-            global password 
-            password = input("Enter yout password(at least 8 symbols): ")
-            choice_encrypt = input("Would you like to encrypt password? (y/n)").lower()
-            encrypt_paassword = choice_encrypt == 'y'
-            valid_passwd = validate_password(password, encrypt_paassword)
-
-            return valid_name, valid_email, valid_passwd
-
-
+    return {"name": valid_name, "email": valid_email, "password": valid_password}
 
 def validate_email(email: str, mask_email: bool) -> str:
-    if not email.strip() or not email.endswith((".com", ".org")) or len(email) < 14 or email.count('@') > 1:
-        print("Write your correct email: ")
-    else:
         try:
-            if mask_email:
-                email, domain = email.split("@")
-                masked_middle = '*' * (len(email) - 2)
-                return f'{email[0]}{masked_middle}{email[-1]}@{domain}'
+            if not email.endswith((".com", ".org")) or len(email) < 14 or email.count('@') > 1:
+                print("Write your correct email: ")
             else:
-                return email
+                if mask_email:
+                    email, domain = email.split("@")
+                    masked_middle = '*' * (len(email) - 2)
+                    return f'{email[0]}{masked_middle}{email[-1]}@{domain}'
+                else:
+                    return email
         except ValueError:
-            print("invalid syntax for email")
-            
-    
-
+            print("incorected data... ")           
 
 def validate_password(password: str, encrypt_paassword: bool) -> str:
-    try:
-        if len(password) > 8 and encrypt_paassword:
-            reversed_paassword = "".join(reversed(password))
-            return f'Your encrypted password: {reversed_paassword}' #ОБОРОТНЕ ШИФРУВАННЯ(?)
-        if len(password) > 8 and not encrypt_paassword:
-            return password
-    except ValueError:
-        print("Too small symbols")
-
+        try:
+            if len(password) > 8:
+                if encrypt_paassword:
+                    reversed_paassword = "".join(reversed(password))
+                    return f'Your encrypted password: {reversed_paassword}'
+                else: 
+                    return password[::1]
+            else:
+                print("Too small symbols")
+        except ValueError:
+            print("Incorrected data entered...")
 
 def validate_name(full_name: str, show_only_initials: bool) -> str:
-    parts = full_name.strip().split() 
-    try:
-        if len(words_in_full_name) >= 2 and len(words_in_full_name[0]) >= 2 and len(words_in_full_name[1]) >= 2:
-            name_initials = " ".join([initials[0] for initials in words_in_full_name])
-            if show_only_initials:
-                return name_initials
-            else:
-                return full_name
-    except ValueError:
-        print("Ops, incorred data entered or your full name is too small. Try again\n")
+        name_parts = full_name.strip().split()
+        try:
+            if len(name_parts) >= 2 and len(name_parts[0]) >= 2 and len(name_parts[1]) >= 2:
+                name_initials = " ".join([initials[0] for initials in name_parts])
+                if show_only_initials:
+                    return name_initials
+                else:
+                    return full_name
+        except ValueError:
+            print("Ops, incorred data entered or your full name is too small. Try again\n")
 
 def main() -> None:
-    exit_choice = input("Welcome to progran!\n Would you want to start? (y/n)").lower()
-    if exit_choice == 'y':
-        user_data = get_user_info()
-        print(f'here you are: {user_data}')
+    exit_choice = input("Welcome to progran!\n Would you want to start? (y/n)").lower() == 'y'
+    if exit_choice:
+        user = get_user_info()
+        print(f'{user["name"]}, \n{user["email"]}, \n{user["password"]}')
     else:
         print("Ok, bye! ")
 
